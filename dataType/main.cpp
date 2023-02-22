f5c922a1915100ae69df4ed73b52bd95ecdee338
lihaoran <1258774709@qq.com>
2023-02-22 17:01:02 +0800

    Initial Commit

---
+++ dataType/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dataType
+//
+//  Created by lihaoran on 2023/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
