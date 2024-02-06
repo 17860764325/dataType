5b94f7d7af421cab737b4d1f38512cc62a9c1a11
lihaoran <1258774709@qq.com>
2024-02-06 17:16:10 +0800

    feat: 排序---冒泡排序

---
+++ dataType/7排序/冒泡排序.cpp
@@ -0,0 +1,28 @@
+//
+// Created by lihaoran on 2024/2/5.
+//
+
+#include "dataTypeWork/冒泡排序.h"
+
+/// 交换方法，每次交换，活进行三次位置移动
+void 冒泡排序::swap(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void 冒泡排序::BubbleSort(int A[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool flag = false;
+        for (int j = n - 1; j > i; j--) {
+            if (A[j - 1] > A[j]) {
+                swap(A[j - 1], A[j]);
+                flag = true;
+            }
+            if (!flag) {
+                return;
+            }
+        }
+    }
+}
---
+++ headSpace/dataTypeWork/冒泡排序.h
@@ -0,0 +1,18 @@
+//
+// Created by lihaoran on 2024/2/5.
+//
+
+#ifndef UNTITLED1_冒泡排序_H
+#define UNTITLED1_冒泡排序_H
+#include "所有的数据结构.h"
+
+
+class 冒泡排序 {
+private:
+public:
+    void BubbleSort(int A[],int n);
+    void swap(int &a,int &b);
+};
+
+
+#endif//UNTITLED1_冒泡排序_H
