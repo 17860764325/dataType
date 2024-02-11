80df509d73085bdedfe321a2956f1eaa7f4cdf97
lihaoran <1258774709@qq.com>
2024-02-11 22:34:06 +0800

    feat: 排序---基数排序的数据结构

--- headSpace/dataTypeWork/所有的数据结构.h
+++ headSpace/dataTypeWork/所有的数据结构.h
@@ -5,30 +5,41 @@
 #ifndef UNTITLED1_所有的数据结构_H
 #define UNTITLED1_所有的数据结构_H
 
-#include "stdio.h"
 #include "iostream"
+#include "stdio.h"
 using namespace std;
 
-typedef int ElemType; /// ElemType 的数据结构类型
-
-/// 二叉树的数据结构常用
-typedef struct BSTNode{
-    int key;
-    struct BSTNode *lchild,*rchild;
-    int count;   /// 栽树的节点中增加一个count 数据成员存储以该节点为根的子树的结点的个数
-} BSTNode,*BSTree;
+typedef int ElemType;/// ElemType 的数据结构类型
 
 /// 单链表
-typedef struct LNode{
-    int data; // 该节点的数据
-    struct LNode *next; // 该节点的下一个节点
-}LNode, *LinkList;
+typedef struct LinkNode {
+    ElemType data;
+    struct LinkNode *next;
+} LinkNode, *LinkList;
+
+/// 二叉树的数据结构常用
+typedef struct BSTNode {
+    int key;
+    struct BSTNode *lchild, *rchild;
+    int count;/// 栽树的节点中增加一个count 数据成员存储以该节点为根的子树的结点的个数
+} BSTNode, *BSTree;
+
+/// 单链表
+typedef struct LNode {
+    int data;          // 该节点的数据
+    struct LNode *next;// 该节点的下一个节点
+} LNode, *LinkList;
 
 /// 排序交换方法：
-void swap(int &a,int &b){
+void swap(int &a, int &b) {
     int temp = a;
-    a =b;
-    b= temp;
+    a = b;
+    b = temp;
 }
+
+/// 基数排序的数据结构
+typedef  struct {
+    LinkNode *front,*rear;/// 队列的队头和队尾指针
+}LinkQueue;
 
 #endif//UNTITLED1_所有的数据结构_H
